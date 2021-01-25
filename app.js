e6c972f40c80062878445948dde25b434e7c00fc
Ankit <ankittrajj1996@gmail.com>
2021-01-26 04:03:17 +0530

    typingTextApp in JS

---
+++ app.js
@@ -0,0 +1,16 @@
+const random_paragraph = ["But just as I didn’t want to resent my kids, I also didn’t want to find myself too much in love with them". 
+"There are parents who don’t like to hear their little girl crying at night, at the vast approaching dark of sleep, and so in their torment think why not feed her a lollipop, and a few years later that kid’s got seven cavities and a pulled tooth."
+" This is how we’ve arrived at the point where we give every kid on the team a trophy in the name of participation." 
+"I didn’t want to love my kids so much that I was blind to their shortcomings, limitations, and mediocre personalities, not to mention character flaws and criminal leanings."
+" But I could, I thought, I could love a kid that much. "
+"A kid really could be everything, and that scared me."
+" Because once a kid is everything, not only might you lose all perspective and start proudly displaying his participation trophies, you might also fear for that kid’s life every time he leaves your sight. I didn’t want to live in perpetual fear."
+" People don’t recover from the death of a child."
+" I knew I wouldn’t. "
+"I knew that having a kid would be my chance to improve upon my shitty childhood, that it would be a repudiation of my dad’s suicide and a celebration of life, but if that kid taught me how to love him, how to love, period, and then I lost him as I lost my dad, that would be it for me."
+ "I’d toss in the towel."
+ " Fuck it, fuck this world and all its heartbreak. I’d tell that to Connie, and she’d tell me that if that was how I felt I was already a slave to the fear, and good luck."];
+
+ let textInput = document.getElementById('text-input');
+ let myText = document.getElementById('mytext'); 
+ let button = document.getElementById('btn');
